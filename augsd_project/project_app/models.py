345e72b037b3f5fb21afc450249995d936ff1a49
2018A7PS0396G <f20180396@goa.bits-pilani.ac.in>
2019-11-02 10:58:14 +0530

    add home route, subSection and Instructor model

--- augsd_project/project_app/models.py
+++ augsd_project/project_app/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Instructor(models.Model):
+    name = models.CharField(max_length = 100)
+
+class SubSection(models.Model):
+    instructor1 = models.ForeignKey(Instructor, related_name='subSection1', default=None)   #backward relationship
+    instructor2 = models.ForeignKey(Instructor, related_name='subSection2', default=None)   #backward relationship
+    days = models.CharField(max_length = 7)
+    startTime = models.IntegerField()
+    endTime = models.IntegerField()
+    # room = models.ForeignKey(Room, related_name='room', default=None)
+    courseCode = models.CharField(max_length = 10)
--- augsd_project/project_app/views.py
+++ augsd_project/project_app/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(*args, **kwargs):
+    return HttpResponse("<h1>Home Page</h1>")
